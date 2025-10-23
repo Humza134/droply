62f5c73c81a101586e90b26b841fb77b780e83cf
Muhammad Hamza <mhumza13420@gmail.com>
2025-10-23 14:31:25 -0700

    add SignUpPage component to render SignUpForm

---
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,10 @@
+import SignUpForm from '@/components/SignUpForm'
+import React from 'react'
+
+export default function SignUpPage() {
+  return (
+    <div className="flex-1 flex justify-center items-center p-6">
+        <SignUpForm />
+    </div>
+  )
+}
