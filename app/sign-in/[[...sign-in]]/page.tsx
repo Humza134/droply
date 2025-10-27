a214c6ba2eeb2f828cdcf97d8b540b99559bd500
Muhammad Hamza <mhumza13420@gmail.com>
2025-10-27 10:35:13 -0700

    add SignInPage component to render SignInForm

---
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,10 @@
+import SignInForm from '@/components/SignInForm'
+import React from 'react'
+
+export default function SignInPage() {
+  return (
+    <div className="flex-1 flex justify-center items-center p-6">
+        <SignInForm />
+    </div>
+  )
+}
